7235f7c945f08101d138907c8a14ee2b84b7fd00
Leonardo <terlizzileonardo@gmail.com>
2022-12-15 13:56:37 -0300

    added validation styles to login form and routes to navbar

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,4 +12,20 @@ export class LoginComponent {
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
   });
+
+  constructor(private router: Router) {}
+
+  login() {
+    console.log('logueando..');
+    console.log(this.loginControl.value);
+    this.router.navigate(['/']);
+  }
+
+  get Email() {
+    return this.loginControl.get('email');
+  }
+
+  get Password() {
+    return this.loginControl.get('password');
+  }
 }
